940924729f14c6c25006d09722f56426ead1d4d1
Dspstudent <103878489+12102163@users.noreply.github.com>
2024-05-13 10:31:03 +0530

    Time: 1 ms (45.15%) | Memory: 41.4 MB (44.85%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.java
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.java
@@ -2,22 +2,11 @@ class Solution {
     public int matrixScore(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        // for(int i[]:grid)System.out.println(Arrays.toString(i));System.out.println("--");
-        // int rowMax=(int)Math.pow(2, m)-1;
-        // int colMax=(int)Math.pow(2, n)-1;
         for(int i=0;i<m;i++){
             if((grid[i][0]==0)){
                 for(int j=0;j<n;j++)grid[i][j]^=1;
             }
         }
-        // int ans=0;
-        // for(int i=0;i<m;i++){
-        //     for(int j=n-1;j>=0;j--){
-        //         if(grid[i][j]==1)
-        //         ans+=(int)Math.pow(2, (n-1)-j);
-        //     }
-        // }
-        // for(int i[]:grid)System.out.println(Arrays.toString(i));
         for(int i=0;i<n;i++){
             int o=0,z=0;
             for(int j=m-1;j>=0;j--){
@@ -36,9 +25,6 @@ class Solution {
                 ans1+=(int)Math.pow(2, (n-1)-j);
             }
         }
-    //    / System.out.println("--");
-        // for(int i[]:grid)System.out.println(Arrays.toString(i));
-        // if(ans1>ans)return ans1;
         return ans1;
     }
 }

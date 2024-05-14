317def58bbb576b66b81286ec189bc36b9f47cf6
Dspstudent <103878489+12102163@users.noreply.github.com>
2024-05-14 17:08:35 +0530

    Time: 94 ms (99.00%) | Memory: 96.6 MB (98.01%) - LeetSync

--- 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.java
+++ 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.java
@@ -1,79 +1,96 @@
-class Node {
-    Node links[];
-    Node(){
-        links=new Node[2];
-    }
-    public boolean containsKey(int i){
-        return links[i]!=null;
-    }
-    public Node getNext(int i){
-        return links[i];
-    }
-    public void addLink(int i){
-        links[i]=new Node();
-    }
-}
 class Solution {
-    Node root;
-    public void insert(int num){
-        Node tri=root;
-        for(int i=31;i>=0;i--){
-            int x=(num >> i)&1;
-            if(!tri.containsKey(x))
-            tri.addLink(x);
-            tri=tri.getNext(x);
-        }
-    }
-//      public  void insert(int num) {
-//     Node tri = root;
-//     for (int i = 31; i >= 0; i--) {
-//       int bit = (num >> i) & 1;
-//       if (!tri.containsKey(bit)) {
-//         tri.addLink(bit);
-//       }
-//       tri = tri.getNext(bit);
-//     }
-//   }
-     public int getMax(int num) {
-    Node node = root;
-    int maxNum = 0;
-    for (int i = 31; i >= 0; i--) {
-      int bit = (num >> i) & 1;
-      if (node.containsKey(1 - bit)) {
-        maxNum = maxNum | (1 << i);
-        node = node.getNext(1 - bit);
-      } else {
-        node = node.getNext(bit);
-      }
-    }
-    return maxNum;
-  }
+
     public int[] maximizeXor(int[] nums, int[][] queries) {
-        root=new Node();
-        Integer q[][]=new Integer[queries.length][3];
-        int it=0;
-        for(int i[]:queries){
-            for(int ind=0;ind<3;ind++){
-                if(ind==2)q[it][2]=it;
-                else
-                q[it][ind]=i[ind];
-            }
-            it++;
-        }
-        Arrays.sort(q, (i, j)->i[1]-j[1]);
         Arrays.sort(nums);
-        it=0;
-        for(Integer i[]:q){
-            System.out.println(Arrays.toString(i));
-        }
-        int ans[]=new int[q.length];
-        for(int i=0;i<q.length;i++){
-            while(it<nums.length && nums[it]<=q[i][1]){
-                insert(nums[it]);it++;
-            }
-            if(it==0)ans[q[i][2]]=-1;
-            else ans[q[i][2]]=getMax(q[i][0]);
-        }
+		pair []p = new pair[queries.length];
+		
+		for(int i=0;i<p.length;i++) {
+			p[i] = new pair(queries[i][0], queries[i][1],i);
+		}
+
+		Arrays.sort(p, new Comparator<pair>(){
+			public int compare(pair o1, pair o2) {
+				return o1.mi-o2.mi;
+			}
+		});
+		
+		int [] ans = new int[p.length];
+		int j=0;
+		Node root = new Node();
+		for(pair x:p) {
+			while(j<nums.length && nums[j]<=x.mi) {
+				Insert(root,nums[j]);
+				j++;
+			}
+			if(j==0)
+				ans[x.i]=-1;
+			else
+					ans[x.i]=getxor(root,x.xi);
+		}
         return ans;
     }
+    public static class Node {
+		Node zero;
+		Node one;
+	}
+	
+	static class pair{
+		int xi,mi,i;
+		public pair(int xi, int mi,int i) {
+			this.xi = xi;
+			this.mi = mi;
+			this.i = i;
+		}
+	}
+
+	public static void Insert(Node root, int val) {
+		Node curr = root;
+		for (int i = 31; i >= 0; i--) {
+			int bit = (val & (1 << i));
+			if (bit == 0) {
+				if (curr.zero != null) {
+					curr = curr.zero;
+				} else {
+					Node nn = new Node();
+					curr.zero = nn;
+					curr = nn;
+				}
+			} else {
+				if (curr.one != null) {
+					curr = curr.one;
+				} else {
+					Node nn = new Node();
+					curr.one = nn;
+					curr = nn;
+
+				}
+			}
+		}
+	}
+
+    private static int getxor(Node root, int val) {
+		// TODO Auto-generated method stub
+		Node curr = root;
+		int xor = 0;
+		for (int i = 31; i >= 0; i--) {
+			int bit = (val & (1 << i));
+			if (bit == 0) {
+				if (curr.one != null) {
+					xor += (1 << i);
+					curr = curr.one;
+				} else {
+					curr = curr.zero;
+				}
+			} else {
+				if (curr.zero != null) {
+					xor += (1 << i);
+					curr = curr.zero;
+				} else {
+					curr = curr.one;
+				}
+			}
+		}
+		return xor;
+	}
+
 }

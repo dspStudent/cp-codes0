8f9e1efd9a12c4935d4712da4e3ee22ad68f7353
Dspstudent <103878489+12102163@users.noreply.github.com>
2024-05-21 07:16:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (16.33%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    private List<List<Integer>>ans;
+    private void f(int i, int a[], ArrayList<Integer>list){
+        if(i>=a.length){
+            ans.add(list);
+            return;
+        }
+        list.add(a[i]);
+        f(i+1, a, new ArrayList<>(list));
+        list.remove(list.size()-1);
+        f(i+1, a, new ArrayList<>(list));
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        ans=new ArrayList<>();
+        f(0, nums, new ArrayList<>());
+        return ans;
+    }
+}

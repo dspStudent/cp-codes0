03428f51189c6f89a87fa2d2a9c23bc201cabeaf
Dspstudent <103878489+dspStudent@users.noreply.github.com>
2024-06-20 09:17:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Integral Points Inside Triangle/integral-points-inside-triangle.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            long p[] = new long[2];
+            long q[] = new long[2];
+            long r[] = new long[2];
+            p[0] = Long.parseLong(S[0]);
+            p[1] = Long.parseLong(S[1]);
+            q[0] = Long.parseLong(S[2]);
+            q[1] = Long.parseLong(S[3]);
+            r[0] = Long.parseLong(S[4]);
+            r[1] = Long.parseLong(S[5]);
+            Solution ob = new Solution();
+            long ans = ob.InternalCount(p, q, r);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // utility function to find GCD of two numbers
+    // GCD of a and b
+    long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    // Finds the no. of Integral points between
+    // two given points.
+    long getBoundaryCount(long p[], long q[]) {
+        // Check if line parallel to axes
+        if (p[0] == q[0]) return Math.abs(p[1] - q[1]) - 1;
+        if (p[1] == q[1]) return Math.abs(p[0] - q[0]) - 1;
+
+        return gcd(Math.abs(p[0] - q[0]), Math.abs(p[1] - q[1])) - 1;
+    }
+
+    // Returns count of points inside the triangle
+    long InternalCount(long p[], long q[], long r[]) {
+        // 3 extra integer points for the vertices
+        long BoundaryPoints = getBoundaryCount(p, q) + getBoundaryCount(p, r) +
+                              getBoundaryCount(q, r) + 3;
+
+        // Calculate 2*A for the triangle
+        long doubleArea = Math.abs(p[0] * (q[1] - r[1]) + q[0] * (r[1] - p[1]) +
+                                   r[0] * (p[1] - q[1]));
+
+        // Use Pick's theorem to calculate the no. of Interior points
+        return (doubleArea - BoundaryPoints + 2) / 2;
+    }
+}

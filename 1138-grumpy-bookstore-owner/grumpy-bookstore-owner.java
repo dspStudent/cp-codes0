823930eb7793c5a1e58067a66ef8946c47b3abd8
Dspstudent <103878489+dspStudent@users.noreply.github.com>
2024-06-21 13:31:22 +0530

    Time: 3 ms (91.49%) | Memory: 47.4 MB (47.57%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -1,19 +1,16 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        int ans = 0, total = 0;
-        for (int i = 0; i < customers.length; i++) {
-            total += (1 - grumpy[i]) * customers[i];
-        }
-
-        int windowAll = 0, windowPartial = 0;
-        for (int i = 0; i < customers.length; i++) {
-            windowAll += customers[i];
-            windowPartial += ((1 - grumpy[i]) * customers[i]);
-            if (i + 1 >= minutes) {
-                ans = Math.max(ans, total - windowPartial + windowAll);
-                int left = i - minutes + 1;
-                windowAll -= customers[left];
-                windowPartial -= ((1 - grumpy[left]) * customers[left]);
+        int n=grumpy.length;
+        int all=0;
+        for(int i=0;i<n;i++)all+=(1-grumpy[i])*customers[i];
+        int winSum=0, winZeroSum=0, ans=0;
+        for(int i=0;i<n;i++){
+            winSum+=customers[i];
+            winZeroSum+=(1-grumpy[i])*customers[i];
+            if(i+1>=minutes){
+                ans=Math.max(ans, all-winZeroSum+winSum);
+                winSum-=customers[i+1 - minutes];
+                winZeroSum-=(1-grumpy[i+1 - minutes])*customers[i+1 - minutes];
             }
         }
         return ans;

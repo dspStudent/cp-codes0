3cabeacd71af5bc4f4bdee63ed56dec667cc49ed
Dspstudent <103878489+dspStudent@users.noreply.github.com>
2024-07-16 09:31:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remaining String/remaining-string.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s = sc.next();
+            char ch = sc.next().charAt(0);
+            int count = sc.nextInt();
+            Solution ob = new Solution();
+            String result = ob.printString(s, ch, count);
+
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public String printString(String s, char ch, int count) {
+        // code here
+        StringBuilder ans=new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(count==0){
+                ans.append(c);
+                continue;
+            }
+            
+            if(c==ch)count--;
+        }
+        return ans.toString();
+    }
+}

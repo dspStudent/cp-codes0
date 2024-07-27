1fcb146a8dc77e7ac3893f01933e505ba6d3e9e4
Dspstudent <103878489+dspStudent@users.noreply.github.com>
2024-07-27 19:23:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            String str = read.readLine();
+
+            Solution ob = new Solution();
+            
+            System.out.println(ob.countMin(str));
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution{
+    static int countMin(String S)
+    {
+        int n = S.length();
+        char[] str = S.toCharArray();
+        
+        // Create a table of size n*n. table[i][j]
+        // will store minumum number of insertions
+        // needed to convert str[i..j] to a palindrome.
+        int table[][] = new int[n][n];
+        int l, h, gap;
+
+        // Fill the table
+        for (gap = 1; gap < n; ++gap)
+        for (l = 0, h = gap; h < n; ++l, ++h)
+            table[l][h] = (str[l] == str[h])?
+                           table[l+1][h-1] :
+                          (Integer.min(table[l][h-1],
+                                 table[l+1][h]) + 1);
+
+        // Return minimum number of insertions
+        // for str[0..n-1]
+        return table[0][n-1];
+    }
+}
+
+ 
